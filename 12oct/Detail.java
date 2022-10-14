9e5e7751e06464e47b7444fd4e9415fb7472ea9c
Abhijit1332 <115772637+Abhijit1332@users.noreply.github.com>
2022-10-13 22:55:16 -0700

    Add files via upload

---
+++ 12oct/Detail.java
@@ -0,0 +1,10 @@
+class Detail
+{
+	public static void main (String []args)
+	{
+		System.out.println("Name :"+ args[0] +" "+args[1]);
+		System.out.println("Age :"+ args[2]);
+		System.out.println("Course :"+ args[3]);
+		System.out.println("score :"+ args[4]);
+	}
+}
