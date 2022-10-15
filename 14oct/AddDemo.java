049693e68854b82755edd445aac5c17dea0f7907
Abhijit1332 <115772637+Abhijit1332@users.noreply.github.com>
2022-10-14 22:16:16 -0700

    Add files via upload

---
+++ 14oct/AddDemo.java
@@ -0,0 +1,10 @@
+class AddDemo
+{
+	public static void main(String []arg)
+	{
+		int num1= Integer.parseInt(arg[0]);
+		int num2= Integer.parseInt(arg[1]);
+		int sum = num1 + num2;
+		System.out.println(" The total no. =  "+sum);
+	}
+}
---
+++ 14oct/DataTypeDemo.java
@@ -0,0 +1,11 @@
+
+class DataTypeDemo
+{
+	public static void main(String []args)
+	{
+		int number1= 20;
+		int number2= 76;
+		int sum = number1+ number2;
+		System.out.println("The sum is :"+sum);
+	}
+}
